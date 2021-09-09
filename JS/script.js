f3a8e8ec370758dd2d8b90464567e8256234e4dd
arman199929 <arm.galstyan.29@gmail.com>
2021-09-09 19:15:58 +0400

    delete task

--- JS/script.js
+++ JS/script.js
@@ -5,13 +5,15 @@ window.addEventListener('load', function () {
     const textArea = document.getElementById('area');
     const button = document.getElementById('submit');
     const addNewTask = document.getElementById('btn');
-    const menu = document.getElementById('menu')
-    addNewTask.addEventListener('click',function (event){
-        if (event){
+    const menu = document.getElementById('menu');
+
+    addNewTask.addEventListener('click', function (event) {
+        if (event) {
             menu.classList.remove('hide')
             menu.style.textAlign = 'center'
         }
     })
+
     class Task {
         constructor(name, description) {
             this.name = name;
@@ -20,12 +22,12 @@ window.addEventListener('load', function () {
         }
 
         toMethod() {
-            return `<div class="task">
-              <li class="taskNumber"> Name - ${this.name}</li>
-              <li class="taskNumber"> Description - ${this.description}</li>
-              <button type="button" class=" btn btn-lg btn-primary">Edit</button>
-              <button type="button" class=" btn btn-lg btn-primary" id="${this.id}">Delete</button>
-             </div>`;
+            return `<li class="task" id="${this.id}">
+              <p class="taskNumber"> Name - ${this.name}</p>
+              <p class="taskNumber"> Description - ${this.description}</p>
+              <button type="button" class=" btn btn-lg btn-primary" data-id="${this.id}">Edit</button>
+              <button type="button" class=" btn btn-lg btn-primary delete" >Delete</button>
+             </li>`;
         }
     }
 
@@ -45,16 +47,30 @@ window.addEventListener('load', function () {
         taskList.push(t);
         render();
     }
+
     function creatTask() {
         const tName = input.value;
         const tDescription = textArea.value;
-        const newTask = new Task(tName,tDescription)
+        const newTask = new Task(tName, tDescription)
         addTask(newTask)
     }
-button.addEventListener('click',creatTask)
 
+    button.addEventListener('click', creatTask)
 
+    function deleteTask(id) {
+        taskList = taskList.filter(function (task) {
+                if (task.id != id)
+                    return task;
+            }
+        );
+        render();
+    }
 
+    const taskItem = document.querySelectorAll('.task');
+
+    taskItem.addEventListener('click', function (event) {
+        console.log(event.target)
+    })
 })
 
 
